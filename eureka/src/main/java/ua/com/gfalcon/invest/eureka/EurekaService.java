02316956f42471b93722f67f38476b97f501cf5f
Oleksii Khalikov <oleksii.khalikov@gmail.com>
2023-04-15 05:10:03 +0200

    feat: EurekaService

--- eureka/src/main/java/ua/com/gfalcon/invest/eureka/EurekaService.java
+++ eureka/src/main/java/ua/com/gfalcon/invest/eureka/EurekaService.java
@@ -14,18 +14,18 @@
  * limitations under the License.
  */
 
-package ua.com.gfalcon.invest.config;
+package ua.com.gfalcon.invest.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableConfigServer
+@EnableEurekaServer
 @SpringBootApplication
-public class ConfigService {
+public class EurekaService {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigService.class, args);
+        SpringApplication.run(EurekaService.class, args);
     }
 
 }
--- eureka/src/test/java/ua/com/gfalcon/invest/eureka/EurekaServiceTest.java
+++ eureka/src/test/java/ua/com/gfalcon/invest/eureka/EurekaServiceTest.java
@@ -14,18 +14,16 @@
  * limitations under the License.
  */
 
-package ua.com.gfalcon.invest.config;
+package ua.com.gfalcon.invest.eureka;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@EnableConfigServer
-@SpringBootApplication
-public class ConfigService {
+@SpringBootTest
+class EurekaServiceTest {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigService.class, args);
+    @Test
+    void contextLoads() {
     }
 
 }
